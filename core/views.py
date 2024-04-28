4c4d8cc16538325580e3c970aae1c0ecf13162b0
H-M-Nizum <hmnizum1714032@gmail.com>
2024-04-28 15:57:01 +0600

    Contact page done

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homeView(request):
     return render(request, 'core/home.html')
+
+def contactView(request):
+    return render(request, 'core/contact.html')
